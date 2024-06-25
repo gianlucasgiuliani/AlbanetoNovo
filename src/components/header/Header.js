8a25041685a630495a54b50e94a642b3c8de40b4
Gianluca S. Giuliani <contatogianlucagiuliani@gmail.com>
2024-06-25 15:26:15 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,12 +28,11 @@ function Header() {
   return (
     <Headroom>
       <header className={isDark ? "dark-menu header" : "header"}>
-         <a href="/" className="logo-image">
-          <img 
-            require="src/assets/images/logo.png" 
+         <img 
+            src={logo}
             alt={greeting.username} 
             className="logo-image"
-          />
+        />
   </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label
