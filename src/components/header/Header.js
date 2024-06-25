1b3692bfb44e0e581d01e03698127d151ac73f48
Gianluca S. Giuliani <contatogianlucagiuliani@gmail.com>
2024-06-25 15:25:39 -0300

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,7 +30,7 @@ function Header() {
       <header className={isDark ? "dark-menu header" : "header"}>
          <a href="/" className="logo-image">
           <img 
-            src={require("../../assets/images/logo.png")} 
+            require="src/assets/images/logo.png" 
             alt={greeting.username} 
             className="logo-image"
           />
