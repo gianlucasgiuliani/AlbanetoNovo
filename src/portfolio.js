7a353e9e6d7b559bf1ea4158f261c660a0611d51
Gianluca S. Giuliani <contatogianlucagiuliani@gmail.com>
2024-07-05 23:52:16 -0300

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -310,20 +310,14 @@ const achievementSection = {
 const blogSection = {
   title: "Blogs",
   subtitle:
-    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+    "Check our latest articles about industry trends.",
   displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://albaneto.medium.com/our-vision-and-mission-statement-4613fadbf3dc",
+      title: "Vision and Mission Statement",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "What moves the people behind Albaneto."
     }
   ],
   display: true // Set false to hide this section, defaults to true
